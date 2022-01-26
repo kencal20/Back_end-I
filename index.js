efa25eaf34404617b8137e82a3e4110a4bfaad2a
kencal20 <kenadjei20@gmail.com>
2022-01-26 10:30:58 +0000

    index update with response

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const http = require("http");
 
 const handleAllRequest = (requestObject, responseObject) => {
   console.log("hi, just recieved a request");
-    console.log(requestObject);
-
+  //   console.log(requestObject);
+  responseObject.write("<h1>this is your response</h1> \n");
+  responseObject.write('second part of response')
+  responseObject.end();
 };
 const server = http.createServer(handleAllRequest);
 
