72f64ccb0de5c2747e4e5a233c2cf432cf70833c
kencal20 <kenadjei20@gmail.com>
2022-01-26 17:20:12 +0000

    express update

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
-const express = require('express');
+const express = require("express");
 
 const handleAllRequest = (requestObject, responseObject) => {
   //<--RequestObject-->
-//   console.log("hi, just recieved a request");
- //   console.log(requestObject);
+  //   console.log("hi, just recieved a request");
+  //   console.log(requestObject);
 
   //  <-- ResponseObject -->
   //   responseObject.write("<h1>this is your response</h1> \n");
   //   responseObject.write('second part of response')
 
   const url = requestObject.url;
-  if (url === "/") {
-    responseObject.write("<h1>Welcome to home page</h1>");
-  } else if (url === "/login") {
-      
-    responseObject.write("<h1>Welcome to Login page</h1>");
-  } else if (url === "/signup") {
-    responseObject.write("<h1>Welcome to Signup page</h1>");
-  }
-  else{
-      responseObject.write("<h1>Page not found</h1>")
-  }
 
   responseObject.end();
 };
-const server = express(handleAllRequest);
-server.use(handleAllRequest)
+
+const handleHomeRequest = (req, res) => {
+  res.send("<h1>Welcome to home page</h1>");
+};
+const handleLoginRequest = (req, res) => {
+  res.send("<h1>Welcome to Login page</h1>");
+};
+const handleSigninRequest = (req, res) => {
+  res.send("<h1>Welcome to Signin page</h1>");
+};
+
+const handleProfileRequest = (req, res) => {
+  res.send("<h1>Welcome to Profile page</h1>");
+};
+
+const server = express();
+server.use("/signin", handleSigninRequest);
+server.use("/login", handleLoginRequest);
+server.use("/profile", handleProfileRequest);
+server.use("/", handleHomeRequest);
+server.use(handleAllRequest);
 
 server.listen(1000, "localhost", () =>
   console.log("server is ready to accept request")
