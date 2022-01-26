73eeb88ee6e318cc5ddd420a4f8769c4dff85df4
kencal20 <kenadjei20@gmail.com>
2022-01-26 11:08:44 +0000

    index url  server update

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const http = require("http");
 
 const handleAllRequest = (requestObject, responseObject) => {
-  console.log("hi, just recieved a request");
-    console.log(requestObject);
+  //<--RequestObject-->
+  //   console.log("hi, just recieved a request");
+  //   console.log(requestObject);
 
+  //  <-- ResponseObject -->
+  //   responseObject.write("<h1>this is your response</h1> \n");
+  //   responseObject.write('second part of response')
+
+  const url = requestObject.url;
+  if (url === "/") {
+    responseObject.write("<h1>Welcome to home page</h1>");
+  } else if (url === "/login") {
+    responseObject.write("<h1>Welcome to Login page</h1>");
+  } else if (url === "/signup") {
+    responseObject.write("<h1>Welcome to Signup page</h1>");
+  } else {
+    responseObject.write("<h1>Page not found</h1>");
+  }
+
+  responseObject.end();
 };
 const server = http.createServer(handleAllRequest);
 
