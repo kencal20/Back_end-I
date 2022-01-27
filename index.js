d43650f4543e476baa8545ca8e6f9225da007f6c
kencal20 <kenadjei20@gmail.com>
2022-01-27 22:27:03 +0000

    express value 'get' update

--- index.js
+++ index.js
@@ -6,11 +6,14 @@ const express = require("express");
 const handleHomeRequest = (req, res) => {
   res.send("<h1>Welcome to Home page</h1>");
 };
-
+const handleLoginRequest = (req, res) => {
+  res.send("<h1>This is the login Page</h1>");
+};
 
 const server = express();
-
+server.get("/login",handleLoginRequest)
 server.use("/", handleHomeRequest);
+
 
 server.listen(1000, "localhost", () =>
   console.log("server is ready to accept request")
