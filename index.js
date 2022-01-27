e62a21441af23481f2a6b0f0f3afd260c474ef20
kencal20 <kenadjei20@gmail.com>
2022-01-27 22:14:43 +0000

    express value intro

--- index.js
+++ index.js
@@ -9,33 +9,11 @@ const handleAllRequest = (requestObject, responseObject) => {
 const handleHomeRequest = (req, res) => {
   res.send("<h1>Welcome to Home page</h1>");
 };
-const handleLoginRequest = (req, res) => {
-  res.send("<h1>Welcome to Login page</h1>");
-};
-const handleSigninRequest = (req, res) => {
-  res.send("<h1>Welcome to Signin page</h1>");
-};
 
-const handleProfileRequest = (req, res) => {
-  res.send("<h1>Welcome to Profile page</h1>");
-};
-
-const handleSettingRequest = (req, res) => {
-  res.send("<h1>Welcome to Settings page</h1>");
-};
-
-const handleContactRequest = (req, res) => {
-  res.send("<h1>Welcome to Contacts page</h1>");
-};
 
 const server = express();
-server.use("/contacts", handleContactRequest);
-server.use("/settings", handleSettingRequest);
-server.use("/signin", handleSigninRequest);
-server.use("/login", handleLoginRequest);
-server.use("/profile", handleProfileRequest);
+
 server.use("/", handleHomeRequest);
-server.use(handleAllRequest);
 
 server.listen(1000, "localhost", () =>
   console.log("server is ready to accept request")
