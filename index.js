8018ca179c424e6eb29e43298ae73e876ea5e46b
kencal20 <kenadjei20@gmail.com>
2022-01-27 22:28:45 +0000

    express value 'post' update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const handleLoginRequest = (req, res) => {
 };
 
 const server = express();
-server.get("/login",handleLoginRequest)
+server.post("/login",handleLoginRequest)
 server.use("/", handleHomeRequest);
 
 
