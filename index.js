f823dd9fffac2e7861f2816a24c05f2b86e76c61
kencal20 <kenadjei20@gmail.com>
2022-01-27 23:47:43 +0000

    express method update(postman)

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const handleSigninRequest = (req, res) => {
   res.send("<h1>This is the Signin Page with Put method</h1>");
 };
 const handleUserRequest = (req, res) => {
-  res.send("<h1>This is the  Page</h1>");
+  res.send("<h1>This is the User Page with method get</h1>");
 };
 const handleContactsRequest = (req, res) => {
   res.send("<h1>This is the contacts Page  with delete method</h1>");
@@ -22,7 +22,7 @@ server.post("/login", handleLoginRequest);
 server.patch("/profile", handleProfileRequest);
 server.put("/signin", handleSigninRequest);
 server.get("/user", handleUserRequest);
-server.delete("/contatcts", handleContactsRequest);
+server.delete("/contacts", handleContactsRequest);
 
 server.listen(1000, "localhost", () =>
   console.log("server is ready to accept request")
