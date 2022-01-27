79e29b892528a439977a3a8389f40ededc0d074c
kencal20 <kenadjei20@gmail.com>
2022-01-27 22:05:15 +0000

    adding routes and request handlers

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 
 const handleAllRequest = (requestObject, responseObject) => {
-
   const url = requestObject.url;
 
   responseObject.end();
 };
 
 const handleHomeRequest = (req, res) => {
-  res.send("<h1>Welcome to home page</h1>");
+  res.send("<h1>Welcome to Home page</h1>");
 };
 const handleLoginRequest = (req, res) => {
   res.send("<h1>Welcome to Login page</h1>");
@@ -21,7 +20,17 @@ const handleProfileRequest = (req, res) => {
   res.send("<h1>Welcome to Profile page</h1>");
 };
 
+const handleSettingRequest = (req, res) => {
+  res.send("<h1>Welcome to Settings page</h1>");
+};
+
+const handleContactRequest = (req, res) => {
+  res.send("<h1>Welcome to Contacts page</h1>");
+};
+
 const server = express();
+server.use("/contacts", handleContactRequest);
+server.use("/settings", handleSettingRequest);
 server.use("/signin", handleSigninRequest);
 server.use("/login", handleLoginRequest);
 server.use("/profile", handleProfileRequest);
