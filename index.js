8c9d92d44d3eb322b2bf688c7d5267e90657e3a2
kencal20 <kenadjei20@gmail.com>
2022-01-29 21:45:26 +0000

    routing files to index

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const path = require("path");
 const server = express("server");
-
-const serveHomePage = (req,res) => {
-  //find the file
-  const homepagefilepath = path.join(__dirname, "public", "index.html");
-  //send file to client
-   res.sendFile(homepagefilepath);
-};
-//Router definition
-server.get("/", serveHomePage);
+server.use(express.static(path.join(__dirname,"public")))
+// const serveHomePage = (req,res) => {
+//   //find the file
+//   const homepagefilepath = path.join(__dirname, "public", "index.html");
+//   //send file to client
+//    res.sendFile(homepagefilepath);
+// };
+// //Router definition
+// server.get("/", serveHomePage);
 
 server.listen(1000, () => console.log("server is ready to accept request"));
