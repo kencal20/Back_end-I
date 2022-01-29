dacf7a904bd9ce8a91b62e46fbb1f4ec0361776d
kencal20 <kenadjei20@gmail.com>
2022-01-29 21:29:30 +0000

    routing to index js

--- index.js
+++ index.js
@@ -1,47 +1,14 @@
 const express = require("express");
-const req = require("express/lib/request");
-const res = require("express/lib/response");
+const path = require("path");
+const server = express("server");
 
-const handleLoginRequest = (req, res) => {
-  res.send("<h1>This is the login Page</h1>");
+const serveHomePage = (req,res) => {
+  //find the file
+  const homepagefilepath = path.join(__dirname, "public", "index.html");
+  //send file to client
+   res.sendFile(homepagefilepath);
 };
-const handleProfileRequest = (req, res) => {
-  res.send("<h1>This is the  Profile Page with patch method</h1>");
-};
+//Router definition
+server.get("/", serveHomePage);
 
-const handleSigninRequest = (req, res) => {
-  res.send("<h1>This is the Signin Page with Put method</h1>");
-};
-const handleUserRequest = (req, res) => {
-  res.send("<h1>This is the User Page with method get</h1>");
-};
-const handleContactsRequest = (req, res) => {
-  res.send("<h1>This is the contacts Page  with delete method</h1>");
-};
-
-
-//Middleware
-const middlewarefunction = (req, res, next) => {
-  // console.log(req);
-  //make some checks
-
-next()
-};
-
-const loginroutespecificmiddleware =(req,res,next) =>{
-  console.log('login route middleware executed');
-  next();
-}
-
-//Routes
-const server = express();
-// server.use(middlewarefunction);
-server.post("/login", loginroutespecificmiddleware,handleLoginRequest);
-server.patch("/profile", handleProfileRequest);
-server.put("/signin", handleSigninRequest);
-server.get("/user", handleUserRequest);
-server.delete("/contacts", handleContactsRequest);
-
-server.listen(1000, "localhost", () =>
-  console.log("server is ready to accept request")
-);
+server.listen(1000, () => console.log("server is ready to accept request"));
