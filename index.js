11fb8cf90dbbfe038e0f860aa18150b6bb7b33cd
kencal20 <kenadjei20@gmail.com>
2022-02-03 09:52:58 +0000

    body parsing login update and correction

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const path = require("path");
 const server = express("server");
 
 const loginRequestHandler = (req, res) => {
+  console.log(req);
   res.send("Done");
 };
 
 server.use(express.static(path.join(__dirname, "public")));
 
-server.post('/',loginRequestHandler)
+server.post("/login", loginRequestHandler);
 
 server.listen(1000, () => console.log("server is ready to accept request"));
